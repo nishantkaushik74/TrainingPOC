86c9fde8b7fca40ed87a59ffda40439e1b60d326
nishant kaushik <kaushik.nishant74@gmail.com>
2023-05-04 06:31:12 +0530

    Auto-generated commit [ci skip]

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    supabaseUrl: 'https://kfuexlloawidengmxlrt.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmdWV4bGxvYXdpZGVuZ214bHJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMxNTc2OTMsImV4cCI6MTk5ODczMzY5M30.vsHvkUu9xxVwVwdoZfKgF7z3B-LNHXLf45fbBJ3CSRM',
-  }
